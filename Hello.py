d82b50eb101584971c6a66f8fb2acdca39b8d24a
jy0604 <143371326+jy0604@users.noreply.github.com>
2023-10-05 19:35:28 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,25 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
+import matplotlib.pyplot as plt
 
-LOGGER = get_logger(__name__)
+# Load your CSV data
+df = pd.read_csv('toy_dataset.csv')
 
+# Group the data
+gen_med = df.groupby(['City', 'Gender'])['Income'].mean().reset_index(name='count')
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Create a Streamlit app
+st.title("Average Income Per City by Gender")
 
-    st.write("# Welcome to Streamlit! 👋")
+# Create a bar plot
+fig, ax = plt.subplots(figsize=(10, 5))
 
-    st.sidebar.success("Select a demo above.")
+# Customize the plot
+ax.bar(gen_med['City'], gen_med['count'])
+ax.set_xlabel("Average Income Per City")
+ax.set_ylabel("Average Income")
+ax.set_title("City")
+plt.xticks(rotation=45, horizontalalignment='right')
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+# Display the plot in Streamlit
+st.pyplot(fig)
